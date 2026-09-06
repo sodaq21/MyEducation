4201d7a3d6cc56de6ccd467ae823e231eb0c2949
sodaq21 <sodaqq21@gmail.com>
2026-09-06 17:05:41 +0300

    Learned difference between value and reference types. Found out about nullable value types.

---
+++ Day1/Day2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value types - structs and enums: int, bool, DateTime, etc.
+            // They contain the actual value. Where they are stored depends on the context.
+
+            // Reference types - classes, arrays, strings, etc.
+            // Variables contain a reference to an object. The object is typically allocated on the heap.
+
+            // Nullable value types <type>?
+            int? x = 5;
+            Console.WriteLine(x.HasValue); // true
+            Console.WriteLine(x.Value); // 5
+            Console.WriteLine(x.GetValueOrDefault()); // 5
+
+            int? y = null;
+            Console.WriteLine(y.HasValue); // false
+            Console.WriteLine(y.Value); // InvalidOperationException
+            Console.WriteLine(y.GetValueOrDefault()); // 0
+            Console.WriteLine(y.GetValueOrDefault(52)); // 52 
+        }
+    }
+}
