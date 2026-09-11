0d4e60b5b8cb622caef8e7c659df51568d439843
sodaq21 <sodaqq21@gmail.com>
2026-09-11 20:28:31 +0300

    Finished Library project

--- Day5/LibraryProject/LibraryProject/Program.cs
+++ Day5/LibraryProject/LibraryProject/Program.cs
@@ -6,7 +6,7 @@ namespace LibraryProject
     class LibraryMember
     {
         private List<LibraryItem> _takenItems = new List<LibraryItem>();
-        private readonly List<string> _levels = new List<string>{ "Basic", "Advanced", "Premium"};
+        private static readonly List<string> _levels = new List<string>{ "Basic", "Advanced", "Premium"};
         public string Name { get; init; }
         private string MembershipLevel { get; set; }
         private int ItemsLimit { get; set; }
@@ -19,46 +19,82 @@ namespace LibraryProject
         }
         public void SetMemberStatus(string level)
         {
-            string current_level = MembershipLevel;
+            string currentLevel = MembershipLevel;
             if (_levels.Contains(level))
             {
+                if (_levels.IndexOf(MembershipLevel) >= _levels.IndexOf(level))
+                {
+                    Console.WriteLine($"You can not change level to the same one or lower it!");
+                    return;
+                }
                 MembershipLevel = level;
-                Console.WriteLine($"Status has been changed: {current_level} -> {level}");
+                Console.WriteLine($"Status has been changed: {currentLevel} -> {level}");
                 SetProperties(level);
             }
             else
-                Console.WriteLine($"There is no {level} level in the list!");
-
+                Console.WriteLine($"The selected level is not on the list!");
         }
 
-        private void SetProperties(string level)
+        public void TakeItem(LibraryItem item)
         {
-            switch (level)
+            foreach (var i in _takenItems)
             {
-                case "Basic":
-                    ItemsLimit = 2;
-                    break;
-                case "Advanced":
-                    ItemsLimit = 5;
-                    break;
-                case "Premium":
-                    ItemsLimit = 10;
-                    break;
-                default:
-                    Console.WriteLine($"{level} level not found.");
-                    break;
+                if (item.Equals(i))
+                {
+                    Console.WriteLine($"You already have taken the {item}!");
+                    return;
+                }
+            }
+            if (_takenItems.Count + 1 <= ItemsLimit)
+            {
+                _takenItems.Add(item);
+                Console.WriteLine($"You have taken the {item.ToString()}!");
+            }
+            else
+            {
+                Console.WriteLine($"You reached your limit ({ItemsLimit}). Upgrade your plan to increase limit!");
             }
         }
+
+        public void ReturnItem(LibraryItem item)
+        {
+            foreach (var i in _takenItems)
+            {
+                if (item.Equals(i))
+                {
+                    _takenItems.Remove(item);
+                    Console.WriteLine($"You have returned the {item.ToString()}!");
+                    return;
+                }
+                
+
+            }
+            Console.WriteLine($"You have not this item!");
+        }
+
+        private void SetProperties(string level) => ItemsLimit = level switch
+        {
+            "Basic" => 2,
+            "Advanced" => 5,
+            "Premium" => 10,
+            _ => ItemsLimit
+        };
 
         public void GetLibraryCard()
         {
+            Console.WriteLine();
             Console.WriteLine($"===={Name}'s library card====");
             Console.WriteLine($"Membership level: {MembershipLevel}");
-            Console.WriteLine($"You can rent {ItemsLimit - _takenItems.Count} more items. (Upgrade your plan to increase limit))");
+            if (MembershipLevel == "Premium")
+                Console.WriteLine($"You can rent {ItemsLimit - _takenItems.Count} more items. (You have reached the maximum level)");
+            else
+                Console.WriteLine($"You can rent {ItemsLimit - _takenItems.Count} more items. (Upgrade your plan to increase limit)");
+
             foreach (var item in _takenItems)
             {
-                Console.WriteLine($"{item.ToString()}: {item.GetInfo()}");
+                Console.WriteLine($"{item}");
             }
+            Console.WriteLine();
         }
     }
 
@@ -67,22 +103,80 @@ namespace LibraryProject
         public string Title { get; init; }
         public string? Author { get; init; }
         public int? Year { get; init; }
-        public int LoanPeriod { get; init; }
 
-        abstract public int GetLoanPeriod();
+        public abstract int GetLoanPeriod();
 
-        public string GetInfo()
+        protected LibraryItem(string title, string? author, int? year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public virtual string GetInfo()
         {
             return $"Title: {Title}, author: {Author}, year: {Year}";
         }
     }
 
+    record class Book : LibraryItem
+    {
+        public Book(string title, string? author, int? year) : base(title, author, year) { }
+
+        public override int GetLoanPeriod()
+        {
+            return 14;
+        }
+    }
+
+    record class DVD : LibraryItem
+    {
+        public DVD(string title, string? author, int? year) : base(title, author, year){ }
+
+        public override int GetLoanPeriod()
+        {
+            return 7;
+        }
+    }
+
+    record class Magazine : LibraryItem
+    {
+        public Magazine(string title, string? author, int? year) : base(title, author, year) { }
+
+        public override int GetLoanPeriod()
+        {
+            return 3;
+        }
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            LibraryMember max = new LibraryMember("Max");
+            DVD dvd = new DVD("5cent Mix", "5cent", 1995);
+            max.GetLibraryCard();
+            max.TakeItem(dvd);
+            max.TakeItem(dvd);
+            max.GetLibraryCard();
+            Book book = new Book("Romeo and Juliet", "William Shakespeare", null);
+            max.TakeItem(book);
+            max.GetLibraryCard();
+            max.ReturnItem(dvd);
+            max.ReturnItem(dvd);
+            max.GetLibraryCard();
+            Magazine magazine = new Magazine("Cooking hints", null, null);
+            max.TakeItem(magazine);
+            max.GetLibraryCard();
+            max.TakeItem(dvd);
+            max.GetLibraryCard();
+            max.SetMemberStatus("Premium");
+            max.GetLibraryCard();
+            max.TakeItem(dvd);
+            max.GetLibraryCard();
+            max.SetMemberStatus("Pro");
+            max.SetMemberStatus("Basic");
+            max.GetLibraryCard();
         }
     }
 }
