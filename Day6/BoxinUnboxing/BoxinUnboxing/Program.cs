c0efb6f10ad130bdbd56d341b1a01b2154d3ace1
sodaq21 <sodaqq21@gmail.com>
2026-09-11 21:17:54 +0300

    Boxing and unboxing were studied

---
+++ Day6/BoxinUnboxing/BoxinUnboxing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BoxingUnboxing
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    struct Point : IPrintable
+    {
+        public int X;
+        public int Y;
+        public Point(int x, int y)
+        {
+            X = x; Y = y;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"X: {X}; Y: {Y}");
+        }
+    }
+
+    internal class Program
+    {
+        static void PrintCheck(IPrintable printable) // any structs objects passed as parameters will be boxed,
+        {                                            // since interfaces store a reference to an object on the heap.        
+            printable.Print();
+        }
+        static void Main(string[] args)
+        {
+            // boxing
+            int a = 5;
+            object b = a; // object is reference type so it allocate memory on heap
+            // unboxing
+            int c = (int)b; // int is value type so it allocate memory on stack
+
+            // decimal d = (decimal)b; // InvalidCastException, because b was boxed to int and it can unbox only to int
+            decimal d = (decimal)(int)b; // can be fixed this way
+
+            Point p = new Point(5, 41);
+            PrintCheck(p);
+        }
+    }
+}
