550bc5f129fd99ee70e47ef561dd781ec0acda26
sodaq21 <sodaqq21@gmail.com>
2026-09-15 22:20:01 +0300

    Starting collections learning and practice.

---
+++ Day8/Collections/Collections/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listInt = [1, 2, 3, 4, 5]; // array shell, not suitable for multiple insert/remove operations
+            LinkedList<int> l = new LinkedList<int>(); // suitable for multiple insert/remove operations
+                                                       // using nodes: each element stores a reference to the previous and the next one
+            Dictionary<int, string> dict = new Dictionary<int, string>(); // <key, value>, provides the opportunity for quick search by key
+        }
+    }
+}
